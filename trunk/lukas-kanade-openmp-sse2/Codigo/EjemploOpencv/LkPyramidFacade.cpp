468729541634e53446b176abd6fb47e515f4cd21
pibgeus <pibgeus@ce9d1b04-dfe2-11dd-ab07-b19b4dad1b3d>
2009-01-15 12:08:24 +0000

    Organizando un poco mas el código fuente

---
+++ trunk/lukas-kanade-openmp-sse2/Codigo/EjemploOpencv/LkPyramidFacade.cpp
@@ -0,0 +1,53 @@
+#include "LkPyramidFacade.h"
+
+void CalcularLKPiramid(IplImage& frameA, IplImage& frameB,CvPoint2D32f frameA_features[],CvPoint2D32f frameB_features[],
+					   int number_of_features, int windowsSize,int level ,float optical_flow_feature_error[],
+					   char optical_flow_found_feature[],algoritmo a)
+{
+	IplImage *eig_image = NULL, *temp_image = NULL, *pyramid1 = NULL, *pyramid2 = NULL;
+	allocateOnDemand( &eig_image,cvGetSize(&frameA), IPL_DEPTH_32F, 1 );
+	allocateOnDemand( &temp_image, cvGetSize(&frameB), IPL_DEPTH_32F, 1 );
+
+	cvGoodFeaturesToTrack(&frameA, eig_image, temp_image, frameA_features, &number_of_features, .01, .01, NULL);
+	CvSize optical_flow_window = cvSize(windowsSize,windowsSize);
+	CvTermCriteria optical_flow_termination_criteria
+		= cvTermCriteria( CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, .3 );
+
+	allocateOnDemand( &pyramid1,cvGetSize(&frameA), IPL_DEPTH_8U, 1 );
+	allocateOnDemand( &pyramid2,cvGetSize(&frameA), IPL_DEPTH_8U, 1 );
+
+	Cronometro c1,c2;
+	int numeroRepeticiones=REPEAT_ALGORITHM_FOR_CLOCK;
+
+	if(a==algoritmo::LKpyramidalClassic)
+	{
+		for(int i=0;i<numeroRepeticiones;i++)
+		{
+			c1.Start();
+			cvCalcOpticalFlowPyrLK(&frameA, &frameB, pyramid1, pyramid2, frameA_features, frameB_features, 
+				number_of_features, optical_flow_window, level, optical_flow_found_feature, optical_flow_feature_error, 
+				optical_flow_termination_criteria, 0 );
+			c1.Stop();
+			c1.Reset();
+		}
+		c1.PrintMinimumTime("");
+	}
+
+	else if(a==algoritmo::LKpyramidalPAA)
+	{
+		for(int i=0;i<numeroRepeticiones;i++)
+		{
+			c2.Start();
+			cvCalcOpticalFlowPyrLK_paa(&frameA, &frameB, pyramid1, pyramid2, frameA_features, frameB_features, 
+				number_of_features, optical_flow_window, level, optical_flow_found_feature, optical_flow_feature_error, 
+				optical_flow_termination_criteria, 0 );
+
+			c2.Stop();
+			c2.Reset();
+		}
+		c2.PrintMinimumTime("");
+	}
+
+	cvReleaseImage(&pyramid1);
+	cvReleaseImage(&pyramid2);
+}
--- trunk/lukas-kanade-openmp-sse2/Codigo/EjemploOpencv/Program.cpp
+++ trunk/lukas-kanade-openmp-sse2/Codigo/EjemploOpencv/Program.cpp
@@ -1,66 +1,8 @@
 #include "common.h"
 #include "video.h"
 #include "gui.h"
-#include <stdlib.h>
-#define REPEAT_ALGORITHM_FOR_CLOCK 1
-
-
-enum algoritmo {LKpyramidalPAA,LKpyramidalClassic};
-enum executionMode{noDisplay,Display};
-
-void CalcularLKPiramid(IplImage& frameA, IplImage& frameB,CvPoint2D32f frameA_features[],CvPoint2D32f frameB_features[],
-					   int number_of_features, int windowsSize,int level ,float optical_flow_feature_error[],
-					   char optical_flow_found_feature[],algoritmo a)
-{
-	IplImage *eig_image = NULL, *temp_image = NULL, *pyramid1 = NULL, *pyramid2 = NULL;
-	allocateOnDemand( &eig_image,cvGetSize(&frameA), IPL_DEPTH_32F, 1 );
-	allocateOnDemand( &temp_image, cvGetSize(&frameB), IPL_DEPTH_32F, 1 );
-
-	cvGoodFeaturesToTrack(&frameA, eig_image, temp_image, frameA_features, &number_of_features, .01, .01, NULL);
-	CvSize optical_flow_window = cvSize(windowsSize,windowsSize);
-	CvTermCriteria optical_flow_termination_criteria
-		= cvTermCriteria( CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, .3 );
-
-	allocateOnDemand( &pyramid1,cvGetSize(&frameA), IPL_DEPTH_8U, 1 );
-	allocateOnDemand( &pyramid2,cvGetSize(&frameA), IPL_DEPTH_8U, 1 );
-
-	Cronometro c1,c2;
-	int numeroRepeticiones=REPEAT_ALGORITHM_FOR_CLOCK;
-
-	if(a==algoritmo::LKpyramidalClassic)
-	{
-		for(int i=0;i<numeroRepeticiones;i++)
-		{
-			c1.Start();
-			cvCalcOpticalFlowPyrLK(&frameA, &frameB, pyramid1, pyramid2, frameA_features, frameB_features, 
-				number_of_features, optical_flow_window, level, optical_flow_found_feature, optical_flow_feature_error, 
-				optical_flow_termination_criteria, 0 );
-			c1.Stop();
-			c1.Reset();
-		}
-		c1.PrintMinimumTime("");
-	}
-
-	else if(a==algoritmo::LKpyramidalPAA)
-	{
-		for(int i=0;i<numeroRepeticiones;i++)
-		{
-			c2.Start();
-			cvCalcOpticalFlowPyrLK_paa(&frameA, &frameB, pyramid1, pyramid2, frameA_features, frameB_features, 
-				number_of_features, optical_flow_window, level, optical_flow_found_feature, optical_flow_feature_error, 
-				optical_flow_termination_criteria, 0 );
-
-			c2.Stop();
-			c2.Reset();
-		}
-		c2.PrintMinimumTime("");
-	}
-
-	cvReleaseImage(&pyramid1);
-	cvReleaseImage(&pyramid2);
-}
-
-
+#include "lkPyramidFacade.h"
+#include "memoryUtils.h"
 
 int EjemploSimple(algoritmo a)
 {
@@ -162,7 +104,6 @@ int EjemploVideo(const char* nombreFichero,algoritmo a,executionMode mode)
 
 	while(continuar)
 	{
-		
 		v.GoToCurrentFrame();		
 		v.GetCurrentFrameCopy(*frame1_1C);
 		v.GetFrameCopy(*frame2_1C,1);
@@ -187,7 +128,7 @@ int EjemploVideo(const char* nombreFichero,algoritmo a,executionMode mode)
 
 
 
-int main(void)
+int main(int argc, char **argv)
 {
 	
 	//char filename[]="tree.avi";
